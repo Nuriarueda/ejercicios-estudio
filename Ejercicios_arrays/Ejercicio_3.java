567c0dacbcd942949cc1d49b22d58d97783e8dae
Nuriarueda <ruedareinanuria@gmail.com>
2023-02-16 20:26:53 +0100

    add

---
+++ Ejercicios_arrays/Ejercicio_3.java
@@ -0,0 +1,13 @@
+/* Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.*/
+
+package Ejercicios_arrays;
+
+public class Ejercicio_3 {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i+=5;
+        } while(i <= 100);
+    }
+}
