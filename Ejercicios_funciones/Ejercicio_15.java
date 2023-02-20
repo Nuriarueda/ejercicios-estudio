0202abcc86be60b0792416a823b5e2d84ea377b0
Nuriarueda <ruedareinanuria@gmail.com>
2023-02-20 10:23:13 +0100

    add

---
+++ Ejercicios_funciones/Ejercicio_15.java
@@ -0,0 +1,35 @@
+/* Realizar una funcion que nos indique en numero de cifras de un numero entero positivo */
+
+package Ejercicios_funciones;
+
+
+import java.util.Scanner;
+public class Ejercicio_15 {
+    
+    public static void main (String[] args) {
+        Scanner sc =  new Scanner (System.in);
+
+        int numero;
+
+        do{
+            System.out.print("Escribe un numero positivo: ");
+            numero = sc.nextInt();
+        } while (numero<0);
+
+        int cifras=cuentaCifras(numero);
+        System.out.println("Hay " + cifras + " cifra/s");
+
+        sc.close();
+    }
+
+    public static int cuentaCifras (int numero){
+        int cifras=1;
+        for (int i=numero; i>10;i/=10) {
+            cifras++;
+        }
+
+        return cifras;
+
+    }
+
+}
