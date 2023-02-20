30a24252be11dc62d369f6a8f3c93073f0c00c52
Nuriarueda <ruedareinanuria@gmail.com>
2023-02-20 09:06:46 +0100

    add

---
+++ Ejercicios_funciones/Ejercicio_11.java
@@ -0,0 +1,33 @@
+/* Factorial del numero 5 */
+
+
+package Ejercicios_funciones;
+
+public class Ejercicio_11 {
+    
+    public static void main(String[] args){
+        System.out.println(factorial(5));
+    }
+
+    public static int factorial(int numero){
+        
+        if(numero<0){
+            
+            System.out.println("No se puede calcular el factorial de un numero negativo");
+            return -1;
+        } 
+
+        if (numero==0 || numero==1) {
+            return 1;
+        }
+
+        int resultado=numero;
+        
+
+        for (int i=(numero-1) ;i>1;i--){
+            resultado*=i;
+        }
+
+        return resultado;
+    }
+}
