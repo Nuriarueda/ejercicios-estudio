bd59e5b0886bb0d22665a56b6765c1880184025a
Nuriarueda <ruedareinanuria@gmail.com>
2023-02-28 11:58:52 +0100

    add

--- Ejercicios_arrays/Simbolos.java
+++ Ejercicios_arrays/Simbolos.java
@@ -1,3 +1,9 @@
+/*Define un array de 10 simboloes con nombre “simbolo” y asigna valores a los elementos 
+según la siguiente tabla:
+Posición 0 1 2 3 4 5 6 7 8 9, valor 'a' 'x' '@' ' ' '+' 'Q'
+Muestra el contenido de todos los elementos del array. ¿Qué sucede con los valores 
+de los elementos que no han sido inicializados?*/
+
 package Ejercicios_arrays;
 
 public class Simbolos {
