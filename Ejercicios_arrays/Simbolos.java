2b885b506ea2b16bdc596576e639a21a4951526a
Nuriarueda <ruedareinanuria@gmail.com>
2023-02-28 11:57:39 +0100

    add

--- Ejercicios_arrays/Simbolos.java
+++ Ejercicios_arrays/Simbolos.java
@@ -10,7 +10,7 @@ public class Simbolos {
         simbolo[7] = '+';
         simbolo[8] = 'Q';
         for (int i = 0; i < 10; i++) {
-        System.out.println("simbolo[" + i + "]: " + simbolo[i]);
+            System.out.println("simbolo[" + i + "]: " + simbolo[i]);
         }
     }
 }
