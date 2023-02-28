9cec3fb9f4668a0e3bf2bf16bf09dc7e9eb02eb2
Nuriarueda <ruedareinanuria@gmail.com>
2023-02-28 12:00:39 +0100

    add

--- Ejercicios_arrays/Inverso.java
+++ Ejercicios_arrays/Inverso.java
@@ -1,3 +1,7 @@
+/* Escribe un programa que lea 10 números por teclado y que luego 
+los muestre en orden inverso, es decir, el primero que se introduce es el último 
+en mostrarse y viceversa. */
+
 package Ejercicios_arrays;
 
 public class Inverso {
