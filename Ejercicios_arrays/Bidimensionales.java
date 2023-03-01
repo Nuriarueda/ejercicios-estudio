0ea318e2c91568dcc8dad68d63cab1cee23a541a
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-01 15:22:31 +0100

    add

--- Ejercicios_arrays/Bidimensionales.java
+++ Ejercicios_arrays/Bidimensionales.java
@@ -4,18 +4,17 @@ package Ejercicios_arrays;
 
 public class Bidimensionales {
     public static void main(String[] args) {
-        System.out.print("\nIntroduzca la posición del alfil, por ejemplo d5: ");
+        System.out.print("Introduzca la posición del alfil, por ejemplo d5: ");
         String posicionAlfil = System.console().readLine();
         int columnaAlfil = (int)(posicionAlfil.charAt(0)) - 96;
         int filaAlfil = (int)(posicionAlfil.charAt(1)) - 48;
         System.out.println("El alfil puede moverse a las siguientes posiciones:");
         for(int fila = 8; fila >= 1; fila--) {
-        for(int columna = 1; columna <= 8; columna++) {
-        if ((Math.abs(filaAlfil - fila) == Math.abs(columnaAlfil - columna))
-        && (! ((filaAlfil == fila) && (columnaAlfil == columna)))) {
-        System.out.print((char)(columna + 96) + "" + fila + " ");
+            for(int columna = 1; columna <= 8; columna++) {
+                if ((Math.abs(filaAlfil - fila) == Math.abs(columnaAlfil - columna)) && (! ((filaAlfil == fila) && (columnaAlfil == columna)))) {
+                    System.out.print((char)(columna + 96) + "" + fila + " ");
+                }
+            }
         }
-        }
-        }
-        }
+    }
 }
