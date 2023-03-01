9b354abc7a91edaa9b52304d538817a5cb483a46
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-01 15:24:28 +0100

    add

--- Ejercicios_arrays/PosicionAfil.java
+++ Ejercicios_arrays/PosicionAfil.java
@@ -9,7 +9,7 @@ public class PosicionAfil {
         String posicionAlfil = System.console().readLine();
         int columnaAlfil = (int)(posicionAlfil.charAt(0)) - 96;
         int filaAlfil = (int)(posicionAlfil.charAt(1)) - 48;
-        System.out.println("El alfil puede moverse a las siguientes posiciones:");
+        System.out.print("El alfil puede moverse a las siguientes posiciones: ");
         for(int fila = 8; fila >= 1; fila--) {
             for(int columna = 1; columna <= 8; columna++) {
                 if ((Math.abs(filaAlfil - fila) == Math.abs(columnaAlfil - columna)) && (! ((filaAlfil == fila) && (columnaAlfil == columna)))) {
