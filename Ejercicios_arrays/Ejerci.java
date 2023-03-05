dc6be0b60f6795cbd21652f450f4db5361d9fa62
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-05 16:24:51 +0100

    add

--- Ejercicios_arrays/Ejerci.java
+++ Ejercicios_arrays/Ejerci.java
@@ -5,13 +5,13 @@ public class Ejerci {
     public static void main(String[] args) {
         int[][] n = new int[9][9];
         for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-        n[i][j] = (int) (Math.random() * 401) + 500;
-        System.out.printf("%5d", n[i][j]);
+            for (int j = 0; j < 9; j++) {
+                n[i][j] = (int) (Math.random() * 401) + 500;
+                System.out.printf("%5d", n[i][j]);
+            }
+            System.out.println();
         }
-        System.out.println();
-        }
-        System.out.print("\nDiagonal desde la esquina inferior izquierda ");
+        System.out.print("Diagonal desde la esquina inferior izquierda ");
         System.out.println("a la esquina superior derecha: ");
         int maximo = 500;
         int minimo = 900;
@@ -20,16 +20,16 @@ public class Ejerci {
             int numero = n[8 - i][i];
             System.out.print(numero + " ");
             if (numero > maximo) {
-            maximo = numero;
+                maximo = numero;
             }
             if (numero < minimo) {
-            minimo = numero;
+                minimo = numero;
             }
             suma += numero;
-            }
-            System.out.println("\nMáximo: " + maximo);
-            System.out.println("Mínimo: " + minimo);
-            System.out.println("Media: " + ((double) suma / 10));
-            }
-            }
+        }
+        System.out.println("\nMáximo: " + maximo);
+        System.out.println("Mínimo: " + minimo);
+        System.out.println("Media: " + ((double) suma / 10));
+    }
 }
+
