4205c151f99ab147d819018dc258a1dfb75acb7e
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-07 12:03:35 +0100

    add

---
+++ Ejercicios_Bucles/Ejercici.java
@@ -0,0 +1,30 @@
+/* Escribe un programa que calcule el factorial de un número entero leído por
+teclado. */
+
+
+package Ejercicios_Bucles;
+
+import java.util.Scanner;
+public class Ejercici {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner (System.in);
+        int numIntroducido;
+        do {
+            System.out.print("Por favor, introduzca un número entero: ");
+            numIntroducido = sc.nextInt();
+            if (numIntroducido < 0) {
+                System.out.println("El número introducido no es correcto.");
+            }
+        } while (numIntroducido < 0);
+        int factorial = numIntroducido;
+        if (numIntroducido == 0) {
+            System.out.println("El factorial del " + numIntroducido + " es 1.");
+        } else {
+            for (int i = 1; i < numIntroducido; i++) {
+                factorial *= i;
+            }
+            System.out.println(numIntroducido + "! = " + factorial);
+        }
+        sc.close();
+    }
+}
