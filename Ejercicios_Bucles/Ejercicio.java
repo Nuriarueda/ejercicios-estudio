fc88fa70c045290d70f93059ee9169a219807f9d
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-07 11:53:24 +0100

    add

---
+++ Ejercicios_Bucles/Ejercicio.java
@@ -0,0 +1,23 @@
+package Ejercicios_Bucles;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        int numeroIntroducido;
+        do {
+            System.out.print("Por favor, introduzca un número entero: ");
+            numeroIntroducido = Integer.parseInt(System.console().readLine());
+            if (numeroIntroducido < 0) {
+                System.out.println("El número introducido no es correcto.");
+            }
+        } while (numeroIntroducido < 0);
+        int factorial = numeroIntroducido;
+        if (numeroIntroducido == 0) {
+            System.out.println("El factorial del " + numeroIntroducido + " es 1.");
+        } else {
+            for (int i = 1; i < numeroIntroducido; i++) {
+                factorial *= i;
+            }
+            System.out.println(numeroIntroducido + "! = " + factorial);
+        }
+    }
+}
