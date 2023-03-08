f21f3d74a6298f63027aa47b86a30fa8c3c1378e
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-08 12:08:23 +0100

    add

---
+++ Ejercicios_Bucles/ForBasico.java
@@ -0,0 +1,10 @@
+package Ejercicios_Bucles;
+
+public class ForBasico {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i + " " );
+        }
+    }
+        
+}
