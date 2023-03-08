9d0bccaa6dd14f4ad6fa0d6c57f4114ad1db2511
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-08 12:10:56 +0100

    add

---
+++ Ejercicios_Bucles/WhileBasico.java
@@ -0,0 +1,13 @@
+/*Bucle While basico del 1-10*/
+
+package Ejercicios_Bucles;
+
+public class WhileBasico {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 11) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
