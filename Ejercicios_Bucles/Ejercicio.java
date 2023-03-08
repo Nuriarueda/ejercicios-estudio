7a107eb23e0ade5dc4badf1e383ebad2f0c4e0e7
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-08 12:11:39 +0100

    add

---
+++ Ejercicios_Bucles/Ejercicio.java
@@ -0,0 +1,11 @@
+package Ejercicios_Bucles;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 11);
+    }
+}
