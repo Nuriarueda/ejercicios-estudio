70eb02ecaf1a63230eb410014441218d89320291
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-08 12:13:27 +0100

    add

---
+++ Ejercicios_Bucles/DoWhileBasico.java
@@ -0,0 +1,13 @@
+/* Bucle Do-While basico del 1-10*/
+
+package Ejercicios_Bucles;
+
+public class DoWhileBasico {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.print(i + " ");
+            i++;
+        } while (i < 11);
+    }
+}
