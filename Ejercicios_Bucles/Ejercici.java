ec1df53d14dcfe7094a1056e773efdf299a73997
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-09 09:43:53 +0100

    add

--- Ejercicios_Bucles/Ejercici.java
+++ Ejercicios_Bucles/Ejercici.java
@@ -7,8 +7,22 @@ public class Ejercici {
         Scanner sc = new Scanner (System.in);
 
         System.out.println("Escribe dos numeros enteros");
-        System.out.println("Numero 1: ");
-        System.out.println("Numero 2: ");
+        System.out.print("Numero 1: ");
+        int num1 = sc.nextInt();
+        System.out.print("Numero 2: ");
+        int num2 = sc.nextInt();
+
+        if (num1 == num2){
+            System.out.print("Tienes que introducir dos numeros distintos");
+        } else if (num1>num2){
+            int aux = num1;
+            num1 = num2;
+            num2 = aux;
+        }
+
+        for ( int i = num1; i < num2; i += 7){
+            System.out.print(i + " ");
+        }
 
         sc.close();
     }
