7d5c4c6e3237076401fd266d1a3f28d88b413b28
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-09 09:35:49 +0100

    add

--- Ejercicios_Bucles/Ejercici.java
+++ Ejercicios_Bucles/Ejercici.java
@@ -1,5 +1,15 @@
 package Ejercicios_Bucles;
 
+import java.util.Scanner;
 public class Ejercici {
     
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Escribe dos numeros enteros");
+        System.out.println("Numero 1: ");
+        System.out.println("Numero 2: ");
+
+        sc.close();
+    }
 }
