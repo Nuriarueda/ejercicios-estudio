b7fcb712d3b6626ed53350e22f8214d47b2a539f
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-13 17:27:44 +0100

    add

---
+++ Ejercicios_Bucles/TablaMultiplicar.java
@@ -0,0 +1,17 @@
+/*  Muestra la tabla de multiplicar de un número introducido por teclado. */
+
+package Ejercicios_Bucles;
+
+
+import java.util.Scanner;
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Introduzca un número entero y le mostraré la tabla de multiplicar: ");
+        int numIntroducido = sc.nextInt();
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(numIntroducido + " x " + i + " = " + numIntroducido * i);
+        }
+        sc.close();
+    }
+}
