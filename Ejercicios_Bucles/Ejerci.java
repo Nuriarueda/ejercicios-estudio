dc06ced7723e4cca184427beca7f19a8b3c42896
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-13 17:23:20 +0100

    add

--- Ejercicios_Bucles/Ejerci.java
+++ Ejercicios_Bucles/Ejerci.java
@@ -3,9 +3,9 @@ package Ejercicios_Bucles;
 public class Ejerci {
     public static void main(String[] args) {
         System.out.print("Introduzca un número entero y le mostraré la tabla de multiplicar: ");
-        int numeroIntroducido = Integer.parseInt(System.console().readLine());
+        int numIntroducido = Integer.parseInt(System.console().readLine());
         for (int i = 0; i <= 10; i++) {
-            System.out.println(numeroIntroducido + " x " + i + " = " + numeroIntroducido * i);
+            System.out.println(numIntroducido + " x " + i + " = " + numIntroducido * i);
         }
     }
 }
