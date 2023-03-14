497ec197867972dea44c0c496d00bbd9782aca55
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-14 12:34:57 +0100

    add

--- Ejercicios_Bucles/CuentaFor.java
+++ Ejercicios_Bucles/CuentaFor.java
@@ -1,9 +1,9 @@
-/* Pide un numero y cuenta hasta el numero pedido por consola */
+/* Pide un numero y cuenta hasta el numero pedido por consola con un bucle FOR */
 
 package Ejercicios_Bucles;
 
 import java.util.Scanner;
-public class Ejercicio25 {
+public class CuentaFor {
     
     public static void main (String[] args){
         Scanner sc = new Scanner (System.in);
