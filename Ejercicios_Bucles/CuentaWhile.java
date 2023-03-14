1ac767407a657fed011058a533359e4dc1ce099d
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-14 12:43:49 +0100

    add

---
+++ Ejercicios_Bucles/CuentaWhile.java
@@ -0,0 +1,22 @@
+/* Pide un numero y cuenta hasta el numero pedido por consola con un bucle WHILE */
+
+
+package Ejercicios_Bucles;
+
+import java.util.Scanner;
+public class CuentaWhile {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Escribe un numero: ");
+        int numero = sc.nextInt();
+
+        int i = 1;
+        while (i<=numero){
+            System.out.print(i + " ");
+            i++;
+        }
+
+        sc.close();
+    }
+}
