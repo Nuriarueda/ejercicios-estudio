26514aeb17fc60746ac907f26c1a4d18d0b557c5
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-14 12:02:17 +0100

    add

---
+++ Ejercicios_funciones/Ejercicio39.java
@@ -0,0 +1,9 @@
+/* Crea una función con la siguiente cabecera: public String convierteEnPalabras(int n)
+Esta función convierte los dígitos del número n en las correspondientes palabras y 
+lo devuelve todo en una cadena de caracteres. */
+
+package Ejercicios_funciones;
+
+public class Ejercicio39 {
+    
+}
