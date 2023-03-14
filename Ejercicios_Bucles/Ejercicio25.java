ec6c9b9cd520f4f0d8d62c6bcb4edd29bdb04907
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-14 12:26:58 +0100

    add

---
+++ Ejercicios_Bucles/Ejercicio25.java
@@ -0,0 +1,15 @@
+
+
+package Ejercicios_Bucles;
+
+import java.util.Scanner;
+public class Ejercicio25 {
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+
+
+
+        sc.close();
+    }
+}
