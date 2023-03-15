eefbb16e80ff4b33197a12221beb24f90a1fec0f
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-15 10:33:32 +0100

    add

--- Ejercicios_Bucles/Ejercicio1.java
+++ Ejercicios_Bucles/Ejercicio1.java
@@ -1,7 +1,16 @@
-/* Pide un numero y cuenta hasta el numero pedido por consola con un bucle W */
+/* Pide un numero y cuenta hasta el numero pedido por consola con un bucle DO WHILE */
 
 package Ejercicios_Bucles;
 
+import java.util.Scanner;
 public class Ejercicio1 {
     
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Escribe un numero entero:");
+
+
+        sc.close();
+    }
 }
