55ef79833e3653b17f4bc9a11910d5e48de3d548
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-15 18:32:06 +0100

    add

---
+++ Ejercicios_Condicionales/SwitchBasico.java
@@ -0,0 +1,8 @@
+/* Escribe un programa que pida por teclado un día de la semana y que diga qué
+asignatura toca a primera hora ese día. */
+
+package Ejercicios_Condicionales;
+
+public class SwitchBasico {
+    
+}
