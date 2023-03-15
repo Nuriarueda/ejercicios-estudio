afe02c1d48140762f78d7c7fdc28d143f9e76a75
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-15 11:04:32 +0100

    add

--- Ejercicios_Bucles/SumaFor.java
+++ Ejercicios_Bucles/SumaFor.java
@@ -1,7 +1,18 @@
-
+/* Haz un programa que sume el numero que te den por consola del 1-10 */
 
 package Ejercicios_Bucles;
 
+import java.util.Scanner;
 public class SumaFor {
     
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Numero que quieras sumar: ");
+        int num = sc.nextInt();
+
+        for (int i=1; i<=10;i++){
+            System.out.println(num + " + " + i + " = "); 
+        }
+        sc.close();
+    }
 }
