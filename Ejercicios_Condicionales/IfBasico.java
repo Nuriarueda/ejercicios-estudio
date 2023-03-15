b06fba3314b228633758039842067fa64a0f0cd3
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-15 18:31:07 +0100

    add

---
+++ Ejercicios_Condicionales/IfBasico.java
@@ -0,0 +1,24 @@
+/* Un programa que te pida cual es su comida favorita y te diga error si no has acertado
+y enhorabuena si has acertado */
+
+package Ejercicios_Condicionales;
+
+import java.util.Scanner;
+public class IfBasico {
+     
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Cual es mi comida favorita: ");
+        String comidafav = sc.nextLine();
+
+        if (comidafav.equals ("hamburguesas")){
+            System.out.println("Enhorabuena, has acertado");
+        } else {
+            System.out.println("Lo siento has fallado");
+        }
+        
+        sc.close();
+
+    }
+}
