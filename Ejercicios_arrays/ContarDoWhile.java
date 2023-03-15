4586e4cc04caff7d99d54efcf1e72f0296f2bdc4
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-15 12:18:53 +0100

    add

--- Ejercicios_arrays/ContarDoWhile.java
+++ Ejercicios_arrays/ContarDoWhile.java
@@ -1,7 +1,7 @@
 /* Muestra los números del 320 al 160, contando de 20 en 20
 utilizando un bucle do-while. */
 
-package Ejercicios_Arrays;
+package Ejercicios_arrays;
 
 public class ContarDoWhile {
     public static void main(String[] args) {
