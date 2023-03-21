31df5e316c54aae9648143e68db0a9ebb24f07b5
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-21 15:39:54 +0100

    add

--- Ejercicios_Condicionales/Ejer.java
+++ Ejercicios_Condicionales/Ejer.java
@@ -1,5 +1,16 @@
 package Ejercicios_Condicionales;
 
+import java.util.Scanner;
 public class Ejer {
-    
+  
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Hola bienvenido a la pasteleria");
+        System.out.print("¿De qué sabor quieres el pastel?: ");
+        String chococlate = sc.nextLine();
+        String vainilla = sc.nextLine();
+
+        if ( )
+        sc.close();
+    }
 }
