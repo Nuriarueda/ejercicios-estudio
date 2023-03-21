6ef46f2ffdaeac377aa9b14cd7a913d19be6621c
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-21 15:46:55 +0100

    add

--- Ejercicios_Condicionales/Ejer.java
+++ Ejercicios_Condicionales/Ejer.java
@@ -10,7 +10,26 @@ public class Ejer {
 
         String sabores = sc.nextLine();
 
-        switch 
+        switch (sabores){
+            case "Chocolate":
+                int chocolate = 15;
+                System.out.println("Le costará 15 euros");
+            break;
+            case "Limon":
+                System.out.println("Le costará 13 euros");
+                int limon = 13;
+            break;
+            case "Vainilla":
+                System.out.println("Le costará 16 euros");
+                int vainilla = 16;
+            break;
+            case "Fresa":
+                System.out.println("Le costará 18 euros");
+                int chocolate = 15;
+            break;
+        }
+
+        System.out.print("Desea añadir ");
         sc.close();
     }
 }
