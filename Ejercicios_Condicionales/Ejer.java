f3cfc47258ab94d15c39d2b126bb7c8543f1ae51
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-21 15:57:06 +0100

    add

--- Ejercicios_Condicionales/Ejer.java
+++ Ejercicios_Condicionales/Ejer.java
@@ -11,14 +11,38 @@ public class Ejer {
         String sabores = sc.nextLine();
 
         switch (sabores){
-            case "Chocolate":
+            case "Chocolate": // SABOR CHOCOLATE
                 int chocolate = 15;
                 System.out.println("Le costará " + chocolate + " euros");
+                System.out.print("¿Desea añadir algun toping extra?: ");
+                String respuesta1 = sc.nextLine();
+
+                if (respuesta1.equals ("si")){
+                    System.out.println("Okey, se le añadira 10 euros al precio de tu pastel");
+                    System.out.println("El precio final sera: " + (chocolate+10) + " euros");
+                } else {
+                    System.out.println("De acuerdo ");
+                }        
+
             break;
+
+
             case "Limon":
             int limon = 13;
                 System.out.println("Le costará " + limon + " euros");
+                System.out.print("¿Desea añadir algun toping extra?: ");
+                String respuesta2 = sc.nextLine();
+
+                if (respuesta2.equals ("si")){
+                    System.out.println("Okey, se le añadira 10 euros al precio de tu pastel");
+                    System.out.println("El precio final sera: " + (limon+10) + " euros");
+                } else {
+                    System.out.println("De acuerdo ");
+                }        
+
             break;
+
+
             case "Vainilla":
                 int vainilla = 16;
                 System.out.println("Le costará " + vainilla + " euros");
@@ -31,7 +55,13 @@ public class Ejer {
 
         System.out.print("¿Desea añadir algun toping extra?: ");
         String respuesta = sc.nextLine();
-        
+
+        if (respuesta.equals ("si")){
+            System.out.println("Okey, se le añadira 10 euros al precio de tu pastel");
+
+        } else {
+            System.out.println("De acuerdo ");
+        }
         sc.close();
     }
 }
