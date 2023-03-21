a34fed90fc5d022f9dd8ffc27e8b1e4316a20b75
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-21 13:52:29 +0100

    add

--- Ejercicios_Condicionales/UltimaCifra.java
+++ Ejercicios_Condicionales/UltimaCifra.java
@@ -3,11 +3,16 @@ introducido por teclado. */
 
 package Ejercicios_Condicionales;
 
+import java.util.Scanner;
 public class UltimaCifra {
     
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
         System.out.print("Por favor, introduzca un número entero: ");
-        int n = Integer.parseInt(System.console().readLine());
+
+        int n = sc.nextInt();
         System.out.println("La última cifra del número introducido es el " + (n % 10));
+        
+        sc.close();
     }
 }
