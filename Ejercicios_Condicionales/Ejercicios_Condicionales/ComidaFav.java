383216bf4d4a729f39814b36d245a018cf197366
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-27 18:11:22 +0200

    add

--- Ejercicios_Condicionales/Ejercicios_Condicionales/ComidaFav.java
+++ Ejercicios_Condicionales/Ejercicios_Condicionales/ComidaFav.java
@@ -1,7 +1,7 @@
 package Ejercicios_Condicionales;
 
 import java.util.Scanner;
-public class Ejercicio3 {
+public class ComidaFav {
     
     public static void main (String [] args){
         Scanner sc = new Scanner (System.in);
