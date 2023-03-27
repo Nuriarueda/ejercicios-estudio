6d8b409b92290146396a8c3ba8fea55a3b5d24cb
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-27 18:12:29 +0200

    add

--- Ejercicios_Condicionales/Ejercicios_Condicionales/ComidaFav.java
+++ Ejercicios_Condicionales/Ejercicios_Condicionales/ComidaFav.java
@@ -6,7 +6,7 @@ public class ComidaFav {
     public static void main (String [] args){
         Scanner sc = new Scanner (System.in);
 
-        System.out.print("Cual es la comida favorita de Maria: ");
+        System.out.print("Cual es la comida favorita de Pedro: ");
 
         String comida = sc.nextLine();
 
