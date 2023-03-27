b261bf58768bc88a4102dd852d2b9d30730d3b25
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-27 18:11:03 +0200

    add

---
+++ Ejercicios_Condicionales/Ejercicios_Condicionales/Ejercicio3.java
@@ -0,0 +1,24 @@
+package Ejercicios_Condicionales;
+
+import java.util.Scanner;
+public class Ejercicio3 {
+    
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Cual es la comida favorita de Nuria: ");
+
+        String comida = sc.nextLine();
+
+        if (comida.equals("Pizza")){
+            System.out.println("Error");
+            
+        } else if (comida.equals("Hamburguesa")){
+            System.out.println("Enhorabuena has acertado!!");
+        }
+        sc.close();
+
+        
+    } 
+
+}
