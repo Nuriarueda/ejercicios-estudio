dd0381005c4379fa7e1e6bbfe3cb24ab1d71c326
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-29 11:22:19 +0200

    add

--- Ejercicio_NumAleatorios/RandomBasico5.java
+++ Ejercicio_NumAleatorios/RandomBasico5.java
@@ -9,7 +9,11 @@ public class RandomBasico5 {
         System.out.println("20 numeros aleatorios entre 50 y 60:");
         
         for (int i = 1; i<=20; i++){
-            System.out.print(((int)(Math.random()*11) + 50) + " ");
+            System.out.print(((int)(Math.random()*11) + 50) + " "); 
         }
     }
 }
+
+//EXPLICACION
+/* En el primer parentesis vamos a generar numeros random de 0-10, a continuacion le sumamos 50 pero FUERA
+del parentesis para que sea de 50 a 60. */
