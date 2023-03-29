a49ae48a7eb1dcd0ef2cce220e94dedf4ad39cb7
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-29 11:07:34 +0200

    add

--- Ejercicio_NumAleatorios/RandomBasico3.java
+++ Ejercicio_NumAleatorios/RandomBasico3.java
@@ -2,13 +2,12 @@ package Ejercicio_NumAleatorios;
 
 public class RandomBasico3 {
     
-
 public static void main (String[] args){
 
     System.out.println("20 numeros aleatorios de 0 - 10:");
 
     for (int i = 1; i<=20; i++){
-        System.out.print((int)(Math.random()*10) + " ");
+        System.out.print((int)(Math.random()*10) + " "); // Aqui no queremos numeros decimales asi que metemos el INT
     }
 } 
 }
