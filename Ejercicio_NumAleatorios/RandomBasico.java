d7734890c04ea058d5830d770d497c02a054c5d3
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-29 11:00:48 +0200

    add

--- Ejercicio_NumAleatorios/RandomBasico.java
+++ Ejercicio_NumAleatorios/RandomBasico.java
@@ -6,7 +6,7 @@ public class RandomBasico {
 
         System.out.println("Diez numeros aleatorios:");
 
-        for (int i = 1; i < 11; i++){
+        for (int i = 1; i < 11; i++){ // En el bucle generamos cuantos numeros nos saldran en este caso 10
             System.out.println(Math.random());
             /* Este Math Rando genereara numeros aleatorios entre 0 - 1 */
         }
