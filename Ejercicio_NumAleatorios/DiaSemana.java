daf644c2086234bcc07085cea2a30123d7e61935
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-29 12:01:59 +0200

    add

--- Ejercicio_NumAleatorios/DiaSemana.java
+++ Ejercicio_NumAleatorios/DiaSemana.java
@@ -1,5 +1,19 @@
 package Ejercicio_NumAleatorios;
 
+
 public class DiaSemana {
     
+    public static void main (String[] args){
+
+        System.out.println("Muestra un dia de la semana al azar: ");
+
+        int dia = (int)(Math.random()*7) + 1;
+
+        switch (dia) {
+
+            case 1:
+                System.out.println("Lunes");
+                break;
+        }
+    }
 }
