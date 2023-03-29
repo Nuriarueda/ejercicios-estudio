2e0bc2c0fabc533178b162bd951c97cc9cda30ec
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-29 11:07:04 +0200

    add

---
+++ Ejercicio_NumAleatorios/RandomBasico3.java
@@ -0,0 +1,14 @@
+package Ejercicio_NumAleatorios;
+
+public class RandomBasico3 {
+    
+
+public static void main (String[] args){
+
+    System.out.println("20 numeros aleatorios de 0 - 10:");
+
+    for (int i = 1; i<=20; i++){
+        System.out.print((int)(Math.random()*10) + " ");
+    }
+} 
+}
