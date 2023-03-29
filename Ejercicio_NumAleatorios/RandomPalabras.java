8b60d04450826ff75a3815bbfa2c9843107ee360
Nuriarueda <ruedareinanuria@gmail.com>
2023-03-29 11:52:58 +0200

    add

--- Ejercicio_NumAleatorios/RandomPalabras.java
+++ Ejercicio_NumAleatorios/RandomPalabras.java
@@ -3,6 +3,9 @@ package Ejercicio_NumAleatorios;
 public class RandomPalabras {
     
     public static void main (String[] args){
+
+        System.out.println("Generea al azar una palabra");
+
         
     }
 }
