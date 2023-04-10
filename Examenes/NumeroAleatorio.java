e9b3982ae64cdb5bd50b32c5ba26147dc494e6e7
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-10 10:32:09 +0200

    add

--- Examenes/NumeroAleatorio.java
+++ Examenes/NumeroAleatorio.java
@@ -1,7 +1,7 @@
 /* Crea una funcion que devuelva un numero aleatorio entre dos numeros 
 enteros que nosotros les demos*/
 
-package Ejercicios_funciones;
+package Examenes;
 
 public class NumeroAleatorio {
     
