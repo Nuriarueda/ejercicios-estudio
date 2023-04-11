e67d20982407e4d182107d86ac85ec24d229410b
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-11 12:41:45 +0200

    add

--- Examenes/Bucles.java
+++ Examenes/Bucles.java
@@ -3,7 +3,9 @@ package Examenes;
 
 public class Bucles {
     
-    public static void
+    public static void main (String [] args) {
+        
+    }
     for (int i = 1; i<=10; i++){
         System.out.println("* ");
     }
