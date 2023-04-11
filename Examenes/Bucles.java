0c3d49b0378b898fb0fe7e78f075c5674266ff89
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-11 12:46:01 +0200

    add

--- Examenes/Bucles.java
+++ Examenes/Bucles.java
@@ -6,8 +6,13 @@ public class Bucles {
     public static void main (String [] args) {
 
         for (int i = 1; i<=10; i++){
-            System.out.println("* ");
-        }    
+            System.out.print("* ");
+            
+        } 
+        for (int j = 1; j<=2; j++){
+            System.out.println("*                    *");
+        }
+      
 
     }
 }
