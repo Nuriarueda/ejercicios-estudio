1703640a18935749243a58de63160eaac764cd6d
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-11 12:51:23 +0200

    add

--- Examenes/Bucles.java
+++ Examenes/Bucles.java
@@ -1,14 +1,15 @@
 package Examenes;
 
 
+import java.util.Scanner;
 public class Bucles {
     
     public static void main (String [] args) {
+        Scanner sc = new Scanner (System.in);
 
-        for (int i = 1; i<=10; i++){
-            System.out.print("* ");
-            
-        }      
+        System.out.print("Escribe la anchura de la carta: ");
 
+
+        sc.close();
     }
 }
