ce83ac33a4d0d6575e19be2891b792c1fa14c566
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-12 11:36:49 +0200

    add

--- Ejercicios_Bucles/Invertir.java
+++ Ejercicios_Bucles/Invertir.java
@@ -5,15 +5,18 @@ public class Invertir {
     public static void main (String[] args){
         int num = 1;
 
-        for (int i=num; i<=6; i++){
-            System.out.print(i + " ");
+        for (int j=1; j<=6;j++){
+            for (int i=num; i<=6; i++){
+                System.out.print(i + " ");
+            }
+    
+            for (int i=num; i>1; i--){
+                System.out.print(i + " ");
+            }
+            num++;
+    
+            System.out.println(j);
         }
-
-        for (int i=num; i>1; i--){
-            System.out.print(i + " ");
-        }
-        num++;
-        
 
     }
 }
