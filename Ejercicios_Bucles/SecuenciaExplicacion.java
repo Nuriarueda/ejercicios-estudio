f6d3b924a83f570cb0157758d2abdbca44d8bb99
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-12 10:56:31 +0200

    add

--- Ejercicios_Bucles/SecuenciaExplicacion.java
+++ Ejercicios_Bucles/SecuenciaExplicacion.java
@@ -4,24 +4,24 @@ public class SecuenciaExplicacion {
     
     public static void main(String[] args) {
 
-        int inicio=1; // Empezamos con uno
+        int principio=1; // Empezamos con uno
 
         for(int i=1; i<=5; i++){
 
-            int siguiente=inicio+1; // Estos serán los números que se imprimirán a continuación del primero
+            int siguiente=principio+1; // Estos serán los números que se imprimirán a continuación del primero
 
-            for(int k=1; k<=i; k++){ // Imprimimos los espacios antes de cada línea
+            for(int j=1; j<=i; j++){ // Imprimimos los espacios antes de cada línea
                 System.out.print(" ");
             }
 
-            System.out.print(inicio+" "); // Imprimimos el primer número
+            System.out.print(principio+" "); // Imprimimos el primer número
 
-            for(int j=1; j<=4; j++){ // A continuación todos los demás, en este caso 4
+            for(int k=1; k<=4; k++){ // A continuación todos los demás, en este caso 4
                 System.out.print(siguiente+" "); // Se imprime
                 siguiente++; // Y sumamos uno
             }
 
-            inicio++; // Hacemos que el número inicial sume una posición
+            principio++; // Hacemos que el número inicial sume una posición
             System.out.println(); // Y saltamos de línea
         }
     }
