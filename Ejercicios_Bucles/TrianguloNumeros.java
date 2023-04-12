a841188be354dff4f107f6565b2e1455de2bc8c0
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-12 12:35:46 +0200

    add

---
+++ Ejercicios_Bucles/TrianguloNumeros.java
@@ -0,0 +1,17 @@
+package Ejercicios_Bucles;
+
+import java.util.Scanner;
+public class TrianguloNumeros {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce el numero hasta donde quieras pintar la piramide: ");
+        int num = sc.nextInt();
+        for (int i = 1; i<=num; i++){
+            System.out.print(i);
+        }
+
+        sc.close();
+    }
+}
