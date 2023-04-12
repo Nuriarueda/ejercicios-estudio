c411075f215f2618ebc7fd1759ea8f3a59fb10e6
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-12 12:21:19 +0200

    add

--- Ejercicios_Bucles/Abecedario.java
+++ Ejercicios_Bucles/Abecedario.java
@@ -6,15 +6,14 @@ public class Abecedario {
 
         char letras;
         char c = 90;
-
+        String espacios = "";
         for (int i = 0; i <= 25; i++){
+            System.out.print(espacios);
             for (letras = c; letras >= 65; letras--){
-
+                System.out.print(letras + " ");
             }
+            c-=1;
+            System.out.println();
         }
-
-
-
-
     }
 }
