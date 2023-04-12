76dda6a8ae6940dac695727e94bbbc0db8c9bec0
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-12 10:54:57 +0200

    add

---
+++ Ejercicios_Bucles/Secuencia.java
@@ -0,0 +1,28 @@
+package Ejercicios_Bucles;
+
+public class Secuencia {
+    
+    public static void main (String [] args){
+
+        int principio = 1;
+        
+        for (int i = 1; i <= 5 ; i++){
+
+            int siguiente = principio+1;
+            
+            for (int j = 1; j<i; j++){
+                System.out.print( " ");
+            }
+
+            System.out.print(principio + " ");
+
+            for (int k = 1; k <= 4; k++ ){
+                System.out.print(siguiente + " ");
+                siguiente ++;
+            }
+
+            principio ++;
+            System.out.println();
+        }
+    }
+}
---
+++ Ejercicios_Bucles/Secuencia2.java
@@ -0,0 +1,28 @@
+package Ejercicios_Bucles;
+
+public class Secuencia2 {
+    
+    public static void main(String[] args) {
+
+        int inicio=1; // Empezamos con uno
+
+        for(int i=1; i<=5; i++){
+
+            int siguiente=inicio+1; // Estos serán los números que se imprimirán a continuación del primero
+
+            for(int k=1; k<=i; k++){ // Imprimimos los espacios antes de cada línea
+                System.out.print(" ");
+            }
+
+            System.out.print(inicio+" "); // Imprimimos el primer número
+
+            for(int j=1; j<=4; j++){ // A continuación todos los demás, en este caso 4
+                System.out.print(siguiente+" "); // Se imprime
+                siguiente++; // Y sumamos uno
+            }
+
+            inicio++; // Hacemos que el número inicial sume una posición
+            System.out.println(); // Y saltamos de línea
+        }
+    }
+}
