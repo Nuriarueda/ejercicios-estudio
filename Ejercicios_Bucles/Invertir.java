52c45707c7be2fbe49878d37ffed509a7645ce02
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-12 11:27:36 +0200

    add

--- Ejercicios_Bucles/Invertir.java
+++ Ejercicios_Bucles/Invertir.java
@@ -3,21 +3,8 @@ package Ejercicios_Bucles;
 public class Invertir {
     
     public static void main (String[] args){
-        int principio = 1;
+        int num = 1;
         
-        for (int i = 1; i <= 5 ; i++){
-
-            int siguiente = principio + 1;
-            
-            System.out.println(" ");
-
-            System.out.print(principio + " ");
-
-            for (int k = 1; k <= 4; k++ ){
-                System.out.print(siguiente + " ");
-                siguiente ++;
-            }
-        }
-
+        
     }
 }
