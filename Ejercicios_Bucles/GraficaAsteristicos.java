0aa4d1b1f3c4fcc1954c2dd76c6e554b4c39a667
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-13 08:59:09 +0200

    add

--- Ejercicios_Bucles/GraficaAsteristicos.java
+++ Ejercicios_Bucles/GraficaAsteristicos.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 public class GraficaAsteristicos {
     
         protected static Scanner sc;
+        static void inicializar(){
+            sc= new Scanner (System.in);
+        }
+
+        public static void finalizar(){
+            sc.close();
+        }
+
         public static long solicitarNumero(){
             long valor = -1;
             System.out.print("Introduce un numero: ");
@@ -21,8 +29,10 @@ public class GraficaAsteristicos {
         }
 
         public static void main (String[] args){
+            inicializar();
             long numero = solicitarNumero();
             System.out.print("Digitos de la cadena " + digitos(numero));
+            finalizar();
         }
 }
 
