bf9491ae962bed1d51e754d936cf830b7d70b557
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-13 08:46:06 +0200

    add

--- Ejercicios_Bucles/GraficaAsteristicos.java
+++ Ejercicios_Bucles/GraficaAsteristicos.java
@@ -5,7 +5,13 @@ public class GraficaAsteristicos {
     
     public static void main (String[] args){
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce un numero que tiene que ser mayor a cero: ");
+        long num;
+
         
+
+
 
 
 
