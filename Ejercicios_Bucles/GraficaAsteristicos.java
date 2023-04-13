776fa5ecafaca53baef18b4119606da5c58353f2
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-13 08:44:22 +0200

    add

--- Ejercicios_Bucles/GraficaAsteristicos.java
+++ Ejercicios_Bucles/GraficaAsteristicos.java
@@ -1,5 +1,15 @@
 package Ejercicios_Bucles;
 
+import java.util.Scanner;
 public class GraficaAsteristicos {
     
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        
+
+
+
+
+        sc.close();
+    }
 }
