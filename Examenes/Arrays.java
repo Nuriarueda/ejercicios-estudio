06635271ebdb8326d283db8cac819ab85fd79c81
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-18 19:29:57 +0200

    add

--- Examenes/Arrays.java
+++ Examenes/Arrays.java
@@ -14,4 +14,23 @@ public class Arrays {
         }
     }
 
+    public static void imprimirTablero(char[][] tablero, int[] posicion, char modo){
+        int i;
+        int j;
+        int longitud = tablero.length;
+        for (i = 0; i < longitud; i++) {
+            for (j = 0; j < longitud; j++) {
+                if (i == 0 || j == 0 || i == longitud - 1 || j == longitud - 1) {
+                    System.out.print("* ");
+                } else if (posicion[1] == i && posicion[0] == j) {
+                    System.out.print(modo + " ");
+                } else {
+                    System.out.print(tablero[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
 }
