213cc09a061eae7bfa47b5292a6dc3cc239f3135
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-20 08:29:07 +0200

    add

--- Alumnado/Alumnos.java
+++ Alumnado/Alumnos.java
@@ -2,5 +2,15 @@ package Alumnado;
 
 public class Alumnos {
     
-    
+    public static int anadirAlumno(String[] alumnos, int cantidad) {
+
+    }
+
+    public static int borrarAlumno(String [] alumnos, int cantidad, int index) {
+
+    }
+
+    public static String modificarAlumno (String[]alumnos, int cantidad, int index) {
+        
+    }
 }
