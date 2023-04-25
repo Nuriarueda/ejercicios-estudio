958fbf0ec8f734466bb6af825e62e2131581d294
Nuriarueda <ruedareinanuria@gmail.com>
2023-04-25 10:23:17 +0200

    add

--- Ejercicios_Bucles/TrianguloNumero.java
+++ Ejercicios_Bucles/TrianguloNumero.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class TrianguloNumeros {
+public class TrianguloNumero {
     
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
